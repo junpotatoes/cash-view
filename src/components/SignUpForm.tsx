fcab4491036b4a35405ad96f0f9c21bd97f768f5
junpotatoes <mukdol1127@naver.com>
2023-02-15 01:50:28 +0900

    #3 - style: styled-components 모듈화

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,5 +1,11 @@
 import { useState } from 'react';
-import styled from 'styled-components';
+import {
+  SignUpWrapper,
+  SignUpHeading,
+  SignUpFormContent,
+  SignUpInput,
+  SignUpButton
+} from '../styles/signup';
 
 interface SignUpFormValues {
   name: string;
@@ -7,76 +13,6 @@ interface SignUpFormValues {
   password: string;
   confirmPassword: string;
 }
-
-const SignUpWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const SignUpHeading = styled.h2`
-  margin-bottom: 1rem;
-`;
-
-const SignUpFormContent = styled.form`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 500px;
-  border-radius: 5px;
-  padding: 1rem;
-
-  .input_box {
-    display: flex;
-    width: 100%;
-    margin-bottom: 15px;
-  }
-
-  label {
-    width: 30%;
-    margin: 0.5rem 0;
-    padding: 0.5rem;
-  }
-
-  .error_box {
-    display: flex;
-    align-item: center;
-    width: 52%;
-  }
-
-  .error_text {
-    color: red;
-    font-size: 8px;
-    margin-top: -15px;
-  }
-`;
-
-const SignUpInput = styled.input`
-  margin: 0.5rem 0;
-  padding: 0.5rem;
-  border-radius: 5px;
-  border: 1px solid ${(props) => props.theme.black};
-  width: 100%;
-`;
-
-const SignUpButton = styled.button`
-  margin: 1rem 0;
-  padding: 0.5rem 1rem;
-  border-radius: 5px;
-  border: none;
-  background-color: ${(props) => props.theme.main};
-  color: ${(props) => props.theme.white};
-  font-size: 1rem;
-  cursor: pointer;
-
-  @media ${(props) => props.theme.desktop} {
-    transition: background-color 0.5s;
-
-    &:hover {
-      background-color: ${(props) => props.theme.hover};
-    }
-  }
-`;
 
 function SignUpForm() {
   const emailCheck =
