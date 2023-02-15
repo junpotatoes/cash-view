2fdc07314cd27e59c645b5889f141b467ecfb828
kimdong-hun <kimdonghun426@gmail.com>
2023-02-16 04:26:08 +0900

    #2 - feat: GNB 컴포넌트 구현

---
+++ src/components/Layout/Header.tsx
@@ -0,0 +1,91 @@
+import { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import * as S from '../../styles/Layout/Header.style';
+import { ReactComponent as MenuIcon } from '../../assets/Icon/menuIcon.svg';
+
+const Header = () => {
+  const path = useLocation().pathname;
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+
+  return (
+    <>
+      <S.HeaderWrapper>
+        <S.HeaderContainer>
+          <h1 className="HeaderTitle">
+            <strong>사용자의</strong>
+            <span>가계부</span>
+          </h1>
+
+          <ul className="HeaderMenuList">
+            <li>
+              <Link
+                to={'/calendar'}
+                className={path === '/calendar' ? 'isActive' : ''}
+              >
+                캘린더
+              </Link>
+            </li>
+            <li>
+              <Link
+                to={'/statistics'}
+                className={path === '/statistics' ? 'isActive' : ''}
+              >
+                월별 통계
+              </Link>
+            </li>
+            <li>
+              <Link
+                to={'/setting'}
+                className={path === '/setting' ? 'isActive' : ''}
+              >
+                설정
+              </Link>
+            </li>
+          </ul>
+
+          <S.HeaderMobileMenu onClick={() => setIsOpenMenu(!isOpenMenu)}>
+            <MenuIcon />
+          </S.HeaderMobileMenu>
+        </S.HeaderContainer>
+      </S.HeaderWrapper>
+
+      <S.MenuModalWrapper
+        onClick={() => setIsOpenMenu(false)}
+        isOpenModal={isOpenMenu}
+      />
+      <S.MenuModalContainer isOpenModal={isOpenMenu}>
+        <ul className="MobileMenuList">
+          <li>
+            <Link
+              to={'/calendar'}
+              className={path === '/calendar' ? 'isActive' : ''}
+              onClick={() => setIsOpenMenu(false)}
+            >
+              캘린더
+            </Link>
+          </li>
+          <li>
+            <Link
+              to={'/statistics'}
+              className={path === '/statistics' ? 'isActive' : ''}
+              onClick={() => setIsOpenMenu(false)}
+            >
+              월별 통계
+            </Link>
+          </li>
+          <li>
+            <Link
+              to={'/setting'}
+              className={path === '/setting' ? 'isActive' : ''}
+              onClick={() => setIsOpenMenu(false)}
+            >
+              설정
+            </Link>
+          </li>
+        </ul>
+      </S.MenuModalContainer>
+    </>
+  );
+};
+
+export default Header;
