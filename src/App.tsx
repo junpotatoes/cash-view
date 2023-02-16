57c0d2fbccaef578d92664f6ade79958dd5fdfd8
Kim Donghun <kimdonghun426@gmail.com>
2023-02-16 11:19:14 +0900

    GNB 컴포넌트 구현 (#8)
    * chore: Icon 추가
    
    * build: Icon 사용 설정
    
    * #2 - feat: GNB 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme.style';
 import { GlobalStyle } from './styles/global.style';
+import Header from './components/Layout/Header';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
 
-      <div>hello world</div>
+      <Header />
     </ThemeProvider>
   );
 };
