b3662ea7509ffb66fef7e4120584d8dfe35eb88f
Kim Donghun <kimdonghun426@gmail.com>
2023-02-20 14:06:00 +0900

    Feature/#12 (#19)
    * #12 - chore: Icon 추가
    
    * design: 레이아웃 구조 수정
    
    * #12 - feat: 캘린더 헤더 컴포넌트 구현

---
+++ src/styles/Layout/Main.style.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const MainWrapper = styled.main`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  margin-top: 50px;
+`;
+
+export const MainContainer = styled.div`
+  width: 100%;
+  max-width: 1200px;
+`;
---
+++ src/styles/calendar/Header/index.style.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const CalendarHeaderContainer = styled.header`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
