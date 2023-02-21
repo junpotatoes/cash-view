77904be6300e79ef3d047510bbec6033e203f812
kimdong-hun <kimdonghun426@gmail.com>
2023-02-21 20:48:31 +0900

    #12 - feat: 캘린더 폼 컴포넌트 구현

---
+++ src/styles/calendar/Main/CalendarMain.style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const CalendarMainWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
+
+export const CalendarMainContainer = styled.div`
+  width: 80%;
+  border-top: 1px solid ${(props) => props.theme.border};
+  border-left: 1px solid ${(props) => props.theme.border};
+
+  @media ${(props) => props.theme.mobile} {
+    border: 1px solid ${(props) => props.theme.border};
+  }
+`;
