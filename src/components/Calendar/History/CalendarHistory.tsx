66f4c0d1c5130f849816d0064565dadaec9da25c
Kim Donghun <kimdonghun426@gmail.com>
2023-02-24 23:15:34 +0900

    JSON server 통신 구현 (#29)
    * #12 - chore: Icon 추가
    
    * design: 레이아웃 구조 수정
    
    * #12 - feat: 캘린더 헤더 컴포넌트 구현
    
    * #12 - chore: Icon 추가
    
    * #12 - feat: 캘린더 헤더 컴포넌트 구현
    
    * #12 - feat: 캘린더 폼 컴포넌트 구현
    
    * #12 - rename: 파일 이름 수정
    
    * #12 - feat: 캘린더 모달 컴포넌트 구현
    
    * #12 - feat: redux-toolkit 적용
    
    * #12 - feat: 캘린더 모달 컴포넌트 구현
    
    * # 12 - feat: 캘린더 내역 컴포넌트 구현
    
    * #12 - feat: JSON server 통신 구현

--- src/components/Calendar/History/CalendarHistory.tsx
+++ src/components/Calendar/History/CalendarHistory.tsx
@@ -1,12 +1,53 @@
 import HistoryComparison from './HistoryComparison';
 import HistoryDetail from './HistoryDetail';
 import * as S from '../../../styles/Calendar/History/CalendarHistory.style';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+type History = {
+  id: number;
+  userId: number;
+  year: number;
+  month: number;
+  date: number;
+  value: string;
+  category: string;
+  amount: number;
+  content: string;
+}[];
+
+export interface HistroyProps {
+  history: History;
+  setHistory?: React.Dispatch<React.SetStateAction<History>>;
+}
 
 function CalendarHistory() {
+  const [history, setHistory] = useState<History>([]);
+
+  const getHistory = async () => {
+    try {
+      const res = await axios.get('http://localhost:4000/historys');
+      setHistory(
+        res.data.filter(
+          (el: any) => el.userId === JSON.parse(localStorage.user).userId
+        )
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getHistory();
+  }, []);
+
   return (
     <S.CalendarHistoryContainer>
-      <HistoryDetail />
-      <HistoryComparison />
+      <HistoryDetail history={history} />
+      <HistoryComparison history={history} />
+      <button type="button" className="addHistoryButton">
+        +
+      </button>
     </S.CalendarHistoryContainer>
   );
 }
--- src/components/Calendar/History/HistoryDetail.tsx
+++ src/components/Calendar/History/HistoryDetail.tsx
@@ -1,51 +1,12 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useAppSelector } from '../../../hooks/store';
 import * as S from '../../../styles/Calendar/History/HistoryDetail.style';
+import { HistroyProps } from './CalendarHistory';
 import HistoryDate from './HistoryDate';
 
-function HistoryDetail() {
-  const history = [
-    {
-      value: '수입',
-      category: '월급',
-      content: '월급 받음',
-      amount: 3000000
-    },
-    {
-      value: '지출',
-      category: '교통',
-      content: '아침에 택시',
-      amount: 9000
-    },
-    {
-      value: '지출',
-      category: '교통',
-      content: '점심에 택시',
-      amount: 9000
-    },
-    {
-      value: '지출',
-      category: '교통',
-      content: '저녁에 택시',
-      amount: 9000
-    },
-    {
-      value: '지출',
-      category: '식비',
-      content: '아침밥',
-      amount: 12000
-    },
-    {
-      value: '지출',
-      category: '식비',
-      content: '점심밥',
-      amount: 12000
-    },
-    {
-      value: '지출',
-      category: '식비',
-      content: '저녁밥',
-      amount: 12000
-    }
-  ];
+function HistoryDetail({ history }: HistroyProps) {
+  const calendar = useAppSelector((state) => state.calendar);
 
   return (
     <S.HistoryDetailContainer>
@@ -55,7 +16,13 @@ function HistoryDetail() {
           <strong className="blue">
             수입{' '}
             {history
-              .filter((el) => el.value === '수입')
+              .filter(
+                (el) =>
+                  el.value === '수입' &&
+                  el.year === calendar.year &&
+                  el.month === calendar.month &&
+                  el.date === calendar.date
+              )
               .map((el) => el.amount)
               .reduce((acc, cur) => {
                 return acc + cur;
@@ -65,7 +32,13 @@ function HistoryDetail() {
           <strong className="red">
             지출{' '}
             {history
-              .filter((el) => el.value === '지출')
+              .filter(
+                (el) =>
+                  el.value === '지출' &&
+                  el.year === calendar.year &&
+                  el.month === calendar.month &&
+                  el.date === calendar.date
+              )
               .map((el) => el.amount)
               .reduce((acc, cur) => {
                 return acc + cur;
@@ -78,19 +51,26 @@ function HistoryDetail() {
       <div className="mainBox">
         <h4 className="mainTitle">내역 상세</h4>
         <ul className="historyList">
-          {history.map((el, idx) => (
-            <li key={idx} className="historyItem">
-              <p className="category">{el.category}</p>
-              <div className="detailContent">
-                <p className="content">{el.content}</p>
-                <strong
-                  className={`amount ${el.value === '수입' ? 'blue' : 'red'}`}
-                >
-                  {el.amount.toLocaleString('ko-KR')}
-                </strong>
-              </div>
-            </li>
-          ))}
+          {history.map(
+            (el, idx) =>
+              el.year === calendar.year &&
+              el.month === calendar.month &&
+              el.date === calendar.date && (
+                <li key={idx} className="historyItem">
+                  <p className="category">{el.category}</p>
+                  <div className="detailContent">
+                    <p className="content">{el.content}</p>
+                    <strong
+                      className={`amount ${
+                        el.value === '수입' ? 'blue' : 'red'
+                      }`}
+                    >
+                      {el.amount.toLocaleString('ko-KR')}
+                    </strong>
+                  </div>
+                </li>
+              )
+          )}
         </ul>
       </div>
     </S.HistoryDetailContainer>
