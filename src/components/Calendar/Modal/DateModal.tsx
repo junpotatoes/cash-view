9d7f326f6c391e54220b8f292b3c900557f3f165
kimdong-hun <kimdonghun426@gmail.com>
2023-02-28 23:13:55 +0900

    #36 - feat: 현재 달의 이전달, 다음달 데이터 바인딩

--- src/components/Calendar/Modal/DateModal.tsx
+++ src/components/Calendar/Modal/DateModal.tsx
@@ -4,15 +4,53 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { useAppDispatch, useAppSelector } from '../../../hooks/store';
 import { changeCalendar } from '../../../store/calendarSlice';
+import { useEffect, useState } from 'react';
 
 function DateModal() {
   const calendar = useAppSelector((state) => state.calendar);
   const dispatch = useAppDispatch();
+  const [dateValue, setDateValue] = useState(new Date());
+
+  const checkCalendar = () => {
+    if (calendar.prevMonthDate) {
+      if (calendar.month === 1) {
+        setDateValue(
+          new Date(`${calendar.year - 1}-${12}-${calendar.prevMonthDate}`)
+        );
+      } else {
+        setDateValue(
+          new Date(
+            `${calendar.year}-${calendar.month - 1}-${calendar.prevMonthDate}`
+          )
+        );
+      }
+    } else if (calendar.nextMonthDate) {
+      if (calendar.month === 12) {
+        setDateValue(
+          new Date(`${calendar.year + 1}-${1}-${calendar.nextMonthDate}`)
+        );
+      } else {
+        setDateValue(
+          new Date(
+            `${calendar.year}-${calendar.month + 1}-${calendar.nextMonthDate}`
+          )
+        );
+      }
+    } else {
+      setDateValue(
+        new Date(`${calendar.year}-${calendar.month}-${calendar.date}`)
+      );
+    }
+  };
+
+  useEffect(() => {
+    checkCalendar();
+  }, [calendar]);
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DatePicker
-        value={new Date(`${calendar.year}-${calendar.month}-${calendar.date}`)}
+        value={dateValue}
         onChange={(newValue: any) => {
           dispatch(
             changeCalendar({
