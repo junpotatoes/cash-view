cb93746e991f611ecf5f47ea85abee9963094b78
junpotatoes <mukdol1127@naver.com>
2023-03-06 00:58:27 +0900

    chore: BASE_URL 수정

--- src/api/customAxios.ts
+++ src/api/customAxios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:4000';
+const BASE_URL =
+  'http://ec2-52-78-60-21.ap-northeast-2.compute.amazonaws.com:8080';
 
 export const baseAPI = axios.create({
   baseURL: BASE_URL
