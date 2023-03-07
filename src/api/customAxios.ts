39e744b38403ce824e88d59b5385fb8f7cc08780
junpotatoes <mukdol1127@naver.com>
2023-03-07 23:07:26 +0900

    chore: BASE_URL 도메인 수정

--- src/api/customAxios.ts
+++ src/api/customAxios.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL =
-  'https://ec2-52-78-60-21.ap-northeast-2.compute.amazonaws.com:8080';
+const BASE_URL = 'https://acc.my-account.p-e.kr:8080';
 
 export const baseAPI = axios.create({
   baseURL: BASE_URL
