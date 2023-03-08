4521935a4c22898fad5472b2a25c6af752bd14bd
junpotatoes <mukdol1127@naver.com>
2023-03-08 22:39:17 +0900

    chore: BASE_URL 업데이트

--- src/api/customAxios.ts
+++ src/api/customAxios.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 import https from 'https';
 
-const BASE_URL =
-  'https://ec2-52-78-60-21.ap-northeast-2.compute.amazonaws.com:8080';
+const BASE_URL = 'https://acc.my-account.p-e.kr';
 const agent = new https.Agent({
   rejectUnauthorized: false
 });
