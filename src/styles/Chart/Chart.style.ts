8cb63da7153e3ad60c425b7e2728ee07f68dfa4d
junpotatoes <mukdol1127@naver.com>
2023-03-10 20:00:17 +0900

    #74 - design: Chart 페이지 CSS 업데이트

--- src/styles/Chart/Chart.style.ts
+++ src/styles/Chart/Chart.style.ts
@@ -29,3 +29,35 @@ export const BarChartConatainer = styled.div`
 export const BarChartBox = styled.div`
   width: 70%;
 `;
+
+export const NoDataBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 500px;
+  border: 1px solid ${(props) => props.theme.gray};
+  border-radius: 5px;
+  font-size: 1.2rem;
+`;
+
+export const NoIncomeDataBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid ${(props) => props.theme.blue};
+  color: ${(props) => props.theme.active};
+  height: 280px;
+  font-size: 0.8rem;
+  margin-right: 10px;
+`;
+
+export const NoOutcomeDataBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid ${(props) => props.theme.red};
+  color: ${(props) => props.theme.red};
+  height: 280px;
+  font-size: 0.8rem;
+  margin-left: 10px;
+`;
