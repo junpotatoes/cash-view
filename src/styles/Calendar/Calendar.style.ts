dcf2cdb6fde36294e635819cd4b11e980335b6e5
김동훈 <kimdonghun426@gmail.com>
2023-03-12 02:10:51 +0900

    #67 - feat: 총 지출/수입 구현 (#82)

--- src/styles/Calendar/Calendar.style.ts
+++ src/styles/Calendar/Calendar.style.ts
@@ -2,26 +2,52 @@ import styled from 'styled-components';
 
 export const CalendarContainer = styled.div`
   display: flex;
-  column-gap: 12px;
-  width: 80%;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
 
-  .calendarBox {
-    width: 100%;
-  }
+  .calendarTop {
+    display: flex;
+    column-gap: 12px;
+    width: 80%;
 
-  .historyBox {
-    flex-shrink: 0;
-    width: 250px;
-    padding-top: 72px;
-  }
-
-  @media ${(props) => props.theme.mobile} {
-    flex-direction: column;
-    row-gap: 12px;
+    .calendarBox {
+      width: 100%;
+    }
 
     .historyBox {
-      width: 100%;
-      padding-top: 0;
+      flex-shrink: 0;
+      width: 250px;
+      padding-top: 72px;
+    }
+
+    @media ${(props) => props.theme.mobile} {
+      flex-direction: column;
+      row-gap: 12px;
+
+      .historyBox {
+        width: 100%;
+        padding-top: 0;
+      }
+    }
+  }
+
+  .calendarBottom {
+    display: flex;
+    align-items: center;
+    column-gap: 48px;
+    width: 80%;
+    padding: 12px 4px;
+
+    @media ${(props) => props.theme.mobile} {
+      flex-direction: column;
+      align-items: flex-start;
+      row-gap: 12px;
+    }
+
+    .amount {
+      font-size: 16px;
+      font-weight: 700;
     }
   }
 `;
